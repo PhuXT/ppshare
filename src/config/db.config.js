dd7c10bc41f0662f0df511afe62640c37ee55bd8
alqueada <Phamphu04042@gmail.com>
2022-04-01 21:20:13 +0700

    fix Database

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+// module.exports = {
+//     async connect(){
+//         try {
+//             await mongoose.connect(process.env.MONGO_CONNECTION_URL)
+//             console.log('DATABASE CONNECTED');
+//         } catch (error) {
+//             console.log('CONNECT FAILED');
+//         }
+//     }
+// }
+
 module.exports = {
-    async connect(){
-        try {
-            await mongoose.connect(process.env.MONGO_CONNECTION_URL)
-            console.log('DATABASE CONNECTED');
-        } catch (error) {
-            console.log('CONNECT FAILED');
-        }
+    connect() {
+        mongoose.connect(process.env.MONGO_CONNECTION_URL)
+
+        const connection = mongoose.connection
+        connection.once('open', () => {
+            console.log('DATABASE connected');
+        }).on('error', err => {
+            console.log('Connection failed');
+        })
     }
 }
